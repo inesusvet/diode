c82fb40145951890cdcc0b6b3d0d02cd7f387dbe
Ivan Nesusvet <ctapbiu.spb@gmail.com>
2013-10-08 01:47:27 -0400

    c9 start

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var fs = require('fs');
-var app = express.createServer();
+var app = express();
+var server = require('http').createServer(app);
 var socket = require('socket.io');
-var io = socket.listen(app);
+var io = socket.listen(server);
 
+server.listen(process.env.PORT, 10, process.env.IP);
 
 var indexhtml;
 
@@ -17,7 +19,7 @@ var index = function(request, response) {
     response.end(indexhtml);
 };
 
-app.route('/', index);
+app.get('/', index);
 
 
 var init_new_client = function(client) {
@@ -31,13 +33,10 @@ var init_new_client = function(client) {
     });
 };
 
-var new_connection = function(client) {
+var new_connection = function(client_socket) {
     console.log('New connection');
 
-    init_new_client(client);
+    init_new_client(client_socket);
 };
 
 io.sockets.on('connection', new_connection);
-
-
-app.listen(8080);
